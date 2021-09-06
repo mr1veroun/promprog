d3c7e448841439801fe723e3c9a653c8b14e854b
doc <vs43573@gmail.com>
2021-09-06 21:07:59 +0300

    hi from ubuntu

--- index.php
+++ index.php
@@ -1,32 +1,35 @@
 <?php
 
 echo "УЕБОК ГО ДБД";
-
+//HI FROM ubuntu
 
 ?>
 
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>KRIPSTER</title>
-<style>
-body{
-margin: 0;
-padding: 0;
-display: flex;
-justify-content: center;
-align-items: center;
-background: #eff;
-font-family: 'Arial';
-font-size: 20px;
-color: #444;
-}
-</style>
+    <meta charset="utf-8">
+    <title>KRIPSTER</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: #eff;
+            font-family: 'Arial';
+            font-size: 20px;
+            color: #444;
+        }
+    </style>
 
 </head>
+
 <body>
-<div class="container">
-<?php echo $result['text']; ?>
-</div>
+    <div class="container">
+        <?php echo $result['text']; ?>
+    </div>
 </body>
+
 </html>
